b6dffee23d0a717465506620ea9ef9fdf6621cf5
Rene <renecmireles@hotmail.com>
2016-11-14 03:12:50 -0600

    Panel de inicio, pacientes y perfil Adicion de formularios para ingresar, eliminar, ver y editar datos

--- src/sql/bd_nutrigo.sql
+++ src/sql/bd_nutrigo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-10-2016 a las 22:14:59
+-- Tiempo de generación: 14-11-2016 a las 10:11:20
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.6.24
 
@@ -23,6 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `fecha` varchar(20) NOT NULL,
+  `hora` varchar(5) NOT NULL,
+  `asunto` varchar(50) NOT NULL,
+  `telefono_cita` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `citas`
+--
+
+INSERT INTO `citas` (`id`, `nombre`, `correo`, `fecha`, `hora`, `asunto`, `telefono_cita`) VALUES
+(6, 'Pedro', 'pe@correo.com', '29/11/2016', '10:00', 'Cita 6', '12312'),
+(7, 'EEE', '321', '17/11/2016', '23:00', '222', '123'),
+(8, 'XXAAAS', 'sdcscscs', '14/11/2016', '08:00', 'bhbhbhb', '122333');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `notas`
 --
 
@@ -30,11 +55,21 @@ CREATE TABLE `notas` (
   `id` int(3) NOT NULL,
   `titulo` varchar(70) NOT NULL,
   `texto` varchar(500) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `R` int(3) NOT NULL,
-  `G` int(3) NOT NULL,
-  `B` int(3) NOT NULL
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `tipo` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `notas`
+--
+
+INSERT INTO `notas` (`id`, `titulo`, `texto`, `fecha`, `hora`, `tipo`) VALUES
+(2, 'Titulo 2', 'Texto 2', '2016-10-28', '00:00:00', 'Alerta'),
+(5, 'Titulo 5.0', 'Texto 5', '2016-10-31', '18:06:15', 'Sin tipo'),
+(6, 'XXXXX', 'XXXXXXXX', '2016-10-29', '00:00:04', 'Sin tipo'),
+(7, 'Titulin', 'Contenido x', '2016-10-29', '02:49:18', 'Urgente'),
+(9, 'Ventanita 3.0', 'Texto de ventanita', '2016-10-31', '18:06:31', 'Suceso');
 
 -- --------------------------------------------------------
 
@@ -66,15 +101,15 @@ CREATE TABLE `pacientes` (
 --
 
 INSERT INTO `pacientes` (`id`, `nombre`, `edad`, `sexo`, `telefono`, `correo`, `celular`, `escolaridad`, `a_heredo`, `a_no_pat`, `a_pat`, `alergias_a`, `alergias_m`, `quirurgicos`, `trastornos`, `suplementos`) VALUES
-(1, 'Rene Castillo', 0, 'Masculino', 1112, 'rene@', 221, 'Primaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0),
+(1, 'Rene Castillo M', 0, 'Masculino', 1112, 'rene@', 221, 'Primaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0),
 (2, 'Jose Perez', 0, 'Masculino', 2221, 'rene@', 1112, 'Primaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0),
 (3, 'Luis Alvarez', 0, 'Masculino', 22210021, 'luis@hotmail.com', 2020202, 'Primaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0),
-(5, 'Martha Sanchez', 0, 'Masculino', 11112, 'martha@hotmail.com', 22222221, 'Primaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0),
 (6, 'Andrea Lopez Cisneros', 30, 'Femenino', 83556100, 'andrea@hotmail.com', 8115203366, 'Posgrado', 'Diabetes', 'Antecedente 1', 'Ninguno', 'Paracetamol', 'Ninguna', 'Fistulas', 'Ninguno', 1),
 (7, 'Lucia Venezuela', 68, 'Femenino', 1234568777, 'lucia@hotm', 1155478547774, 'Primaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0),
 (8, 'Luis Jaime Ortiz', 34, 'Masculino', 1102236554, 'lj@hotmail.com', 222012365554, 'Primaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0),
 (9, 'Martha Castillo Mireles', 17, 'Femenino', 83825719, 'martha1@hotmail.com', 811269325, 'Bachiller', 'Hipertension', 'Hipertension', 'Alcoholismo', 'paracetamol', '', 'Ileostoias', 'Extreñimiento', 1),
-(10, 'Mayra Alejandra Villareal Salazar', 29, 'Femenino', 83133182, 'mayra_latermita@hotamial.com', 8116300282, 'Licenciatura', 'Hipertension', 'Ninguno', 'Ninguno', 'aspirina junior, acido folico', 'Ninguna', 'Ninguno', 'Extreñimiento', 1);
+(10, 'Mayra Alejandra Villareal Salazar', 29, 'Femenino', 83133182, 'mayra_latermita@hotamial.com', 8116300282, 'Licenciatura', 'Hipertension', 'Ninguno', 'Ninguno', 'aspirina junior, acido folico', 'Ninguna', 'Ninguno', 'Extreñimiento', 1),
+(12, 'jose martinez', 21, 'Masculino', 12541, '', 125563, 'Secundaria', 'Ninguno', 'Ninguno', 'Ninguno', 'Ninguna', 'Ninguna', 'Ninguno', 'Ninguno', 0);
 
 -- --------------------------------------------------------
 
@@ -125,24 +160,23 @@ CREATE TABLE `pacientes_clinico` (
   `piel_7` tinyint(1) NOT NULL,
   `esqueleto_1` tinyint(1) NOT NULL,
   `esqueleto_2` tinyint(1) NOT NULL,
-  `esqueleto_3` tinyint(1) NOT NULL,
-  `gastro_1` tinyint(1) NOT NULL
+  `esqueleto_3` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `pacientes_clinico`
 --
 
-INSERT INTO `pacientes_clinico` (`id`, `edema`, `obesidad`, `unas_1`, `unas_2`, `cuello_1`, `cuello_2`, `nervioso_1`, `nervioso_2`, `nervioso_3`, `rostro_1`, `rostro_2`, `rostro_3`, `dientes_1`, `dientes_2`, `dientes_3`, `urinaria_1`, `urinaria_2`, `torax_1`, `torax_2`, `torax_3`, `torax_4`, `boca_1`, `boca_2`, `boca_3`, `boca_4`, `boca_5`, `boca_6`, `ojos_1`, `ojos_2`, `ojos_3`, `ojos_4`, `ojos_5`, `piel_1`, `piel_2`, `piel_3`, `piel_4`, `piel_5`, `piel_6`, `piel_7`, `esqueleto_1`, `esqueleto_2`, `esqueleto_3`, `gastro_1`) VALUES
-(1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(6, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0),
-(10, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+INSERT INTO `pacientes_clinico` (`id`, `edema`, `obesidad`, `unas_1`, `unas_2`, `cuello_1`, `cuello_2`, `nervioso_1`, `nervioso_2`, `nervioso_3`, `rostro_1`, `rostro_2`, `rostro_3`, `dientes_1`, `dientes_2`, `dientes_3`, `urinaria_1`, `urinaria_2`, `torax_1`, `torax_2`, `torax_3`, `torax_4`, `boca_1`, `boca_2`, `boca_3`, `boca_4`, `boca_5`, `boca_6`, `ojos_1`, `ojos_2`, `ojos_3`, `ojos_4`, `ojos_5`, `piel_1`, `piel_2`, `piel_3`, `piel_4`, `piel_5`, `piel_6`, `piel_7`, `esqueleto_1`, `esqueleto_2`, `esqueleto_3`) VALUES
+(1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(6, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0),
+(10, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(12, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -153,45 +187,36 @@ INSERT INTO `pacientes_clinico` (`id`, `edema`, `obesidad`, `unas_1`, `unas_2`, 
 CREATE TABLE `pacientes_evaluacion` (
   `id` int(3) NOT NULL,
   `hora_1` varchar(15) NOT NULL,
-  `lugar_1` varchar(40) NOT NULL,
   `alimento_1` varchar(50) NOT NULL,
   `ingredientes_1` varchar(400) NOT NULL,
   `hora_2` varchar(15) NOT NULL,
-  `lugar_2` varchar(40) NOT NULL,
   `alimento_2` varchar(50) NOT NULL,
   `ingredientes_2` varchar(400) NOT NULL,
   `hora_3` varchar(15) NOT NULL,
-  `lugar_3` varchar(40) NOT NULL,
   `alimento_3` varchar(50) NOT NULL,
   `ingredientes_3` varchar(400) NOT NULL,
   `hora_4` varchar(15) NOT NULL,
-  `lugar_4` varchar(40) NOT NULL,
   `alimento_4` varchar(50) NOT NULL,
   `ingredientes_4` varchar(400) NOT NULL,
   `hora_5` varchar(15) NOT NULL,
-  `lugar_5` varchar(40) NOT NULL,
-  `argumento_5` varchar(50) NOT NULL,
-  `ingredientes_5` varchar(400) NOT NULL,
-  `hora_6` varchar(15) NOT NULL,
-  `lygar_6` varchar(40) NOT NULL,
-  `alimento_6` varchar(50) NOT NULL,
-  `ingredientes_6` varchar(400) NOT NULL
+  `alimento_5` varchar(50) NOT NULL,
+  `ingredientes_5` varchar(400) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `pacientes_evaluacion`
 --
 
-INSERT INTO `pacientes_evaluacion` (`id`, `hora_1`, `lugar_1`, `alimento_1`, `ingredientes_1`, `hora_2`, `lugar_2`, `alimento_2`, `ingredientes_2`, `hora_3`, `lugar_3`, `alimento_3`, `ingredientes_3`, `hora_4`, `lugar_4`, `alimento_4`, `ingredientes_4`, `hora_5`, `lugar_5`, `argumento_5`, `ingredientes_5`, `hora_6`, `lygar_6`, `alimento_6`, `ingredientes_6`) VALUES
-(1, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(2, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(3, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(5, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(6, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(7, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(8, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(9, '13:00:00', 'Casa', 'huevo con jamon', 'huevo\njamon\naceite', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', ''),
-(10, '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '', '0:00:00', '', '', '');
+INSERT INTO `pacientes_evaluacion` (`id`, `hora_1`, `alimento_1`, `ingredientes_1`, `hora_2`, `alimento_2`, `ingredientes_2`, `hora_3`, `alimento_3`, `ingredientes_3`, `hora_4`, `alimento_4`, `ingredientes_4`, `hora_5`, `alimento_5`, `ingredientes_5`) VALUES
+(1, '0:00:05', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(2, '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(3, '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(6, '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(7, '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(8, '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(9, '13:00:00', 'huevo con jamon', 'huevo\njamon\naceite', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(10, '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', ''),
+(12, '10:00:00', 'sanwich', '2 rebanadas pan\n1 rebanada de jamon\n1 rebanada de queso\n', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '', '0:00:00', '', '');
 
 -- --------------------------------------------------------
 
@@ -209,37 +234,26 @@ CREATE TABLE `pacientes_info` (
   `c_brazo` float NOT NULL,
   `biceps` float NOT NULL,
   `triceps` float NOT NULL,
-  `subescapular` float NOT NULL,
-  `ileocrestal` float NOT NULL,
-  `suprailiaco` float NOT NULL,
-  `hemoglobina` float NOT NULL,
-  `hematrocitos` float NOT NULL,
-  `linfocitos` float NOT NULL,
   `glucosa` float NOT NULL,
-  `urea` float NOT NULL,
   `a_urico` float NOT NULL,
-  `creatonina` float NOT NULL,
   `trigliceridos` float NOT NULL,
-  `colesterol` float NOT NULL,
-  `albumina` float NOT NULL,
-  `sodio` float NOT NULL,
-  `potasio` float NOT NULL
+  `colesterol` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `pacientes_info`
 --
 
-INSERT INTO `pacientes_info` (`id`, `peso`, `estatura`, `c_cintura`, `c_cadera`, `c_muneca`, `c_brazo`, `biceps`, `triceps`, `subescapular`, `ileocrestal`, `suprailiaco`, `hemoglobina`, `hematrocitos`, `linfocitos`, `glucosa`, `urea`, `a_urico`, `creatonina`, `trigliceridos`, `colesterol`, `albumina`, `sodio`, `potasio`) VALUES
-(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-(2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-(3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-(5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4),
-(6, 90, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2),
-(7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-(8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-(9, 82, 162, 23, 100, 100, 26, 12, 12, 12, 22, 25, 12, 12, 20, 100, 22, 22, 1.2, 150, 15, 22, 33, 22),
-(10, 56, 155, 70, 100, 100, 17, 10, 10, 10, 10, 10, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12);
+INSERT INTO `pacientes_info` (`id`, `peso`, `estatura`, `c_cintura`, `c_cadera`, `c_muneca`, `c_brazo`, `biceps`, `triceps`, `glucosa`, `a_urico`, `trigliceridos`, `colesterol`) VALUES
+(1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+(2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+(3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+(6, 90, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2),
+(7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+(8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+(9, 82, 162, 23, 100, 100, 26, 12, 12, 100, 22, 150, 15),
+(10, 56, 155, 70, 100, 100, 17, 10, 10, 12, 12, 12, 12),
+(12, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -280,6 +294,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(50) NOT NULL,
   `correo` varchar(30) NOT NULL,
   `pass` varchar(35) NOT NULL,
+  `telefono` bigint(15) NOT NULL,
   `sexo` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -287,12 +302,18 @@ CREATE TABLE `usuarios` (
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`usuario`, `nombre`, `correo`, `pass`, `sexo`) VALUES
-('Usuario_1', 'Jose Perez Torres', 'juanp@hotmail.com', '202cb962ac59075b964b07152d234b70', 'M');
+INSERT INTO `usuarios` (`usuario`, `nombre`, `correo`, `pass`, `telefono`, `sexo`) VALUES
+('Usuario_1', 'Jose Perez Gonzalez', 'juanp@hotmail.es', '202cb962ac59075b964b07152d234b70', 8114523366, 'M');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `notas`
@@ -341,30 +362,35 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
 -- AUTO_INCREMENT de la tabla `notas`
 --
 ALTER TABLE `notas`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `pacientes_clinico`
 --
 ALTER TABLE `pacientes_clinico`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `pacientes_evaluacion`
 --
 ALTER TABLE `pacientes_evaluacion`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `pacientes_info`
 --
 ALTER TABLE `pacientes_info`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT de la tabla `prueba`
 --
